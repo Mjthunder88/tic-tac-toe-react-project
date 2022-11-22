81c51e9a9ebfb1f341eab06c2e3911147bbd6743
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2022-11-22 10:19:51 -0700

    started week 1 tic tac toe project and finished the first 2 steps

---
+++ src/components/Square.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Square = (props) => {
+    return (
+        <div className='square'>
+            <h2>{props.name}</h2>
+        </div>
+    )
+}
+
+export default Square
