9f9b47284088b90ffda5a06ae0b58ade9d618e5c
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2022-11-23 16:06:15 -0700

    finished step 4 for the tic tac toe project which renders the board and displays the X's and O's

--- src/App.js
+++ src/App.js
@@ -3,14 +3,32 @@ import Square from './components/Square';
 import './App.css';
 
 function App() {
-  const person = {
-    name: "Marcus"
-  }
+  // const person = {
+  //   name: "Marcus"
+  // }
   const [gameSquares, updateGameSquares] = useState(['', '', '', '',  '', '', '', '', '']);
   const [player, updatePlayer] = useState(true);
+
+  const handleClick = () => {
+    updateGameSquares(['', '', '', '', '', '', '', '', '',]);
+    updatePlayer(true);
+  }
+
   return (
     <div className="App">
-      <Square squares={gameSquares} setSquares={updateGameSquares} player={player} setPlayer={updatePlayer} />
+      <div className='container'>
+        {gameSquares.map((square, index) => {
+          return <Square 
+          squares={gameSquares} 
+          setSquares={updateGameSquares} 
+          player={player} 
+          setPlayer={updatePlayer}
+          squareValue={square}
+          index={index}
+          />
+        })}
+      </div>
+      <button onClick={handleClick}>Reset</button>
     </div>
   );
 }
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -3,10 +3,19 @@ import React from 'react'
 const Square = (props) => {
     console.log(props.squares, props.player)
     // console.log(props.player)
+
+    const handleClick = () => {
+        return !props.squareValue ? 
+        (props.squares.splice(props.index, 1, 'X'), props.setSquares(props.squares), props.setPlayer(!props.player)) : 
+        (props.squares.splice(props.index, 1, 'O'), props.setSquares(props.squares), props.setPlayer(!props.player)) 
+    }
+
     return (
-        <div className='square'>
+        <div className='square' onClick={handleClick}>
+            {props.squareValue === 'O' ? <img src='https://cdn.discordapp.com/attachments/830137099042816080/984895322184634448/devcircle_1.png' alt='dev_mountain_logo' /> : props.squareValue}
         </div>
     )
 }
+
 
 export default Square
