b3dc413f3f8c5faa5dfd50a246c60e62bbcdb24e
Marcus Johnson <marcusjohnson88.mj@gmail.com>
2022-11-27 17:33:00 -0700

    finished tic tac toe project for week 9 react

--- src/App.js
+++ src/App.js
@@ -14,8 +14,23 @@ function App() {
     updatePlayer(true);
   }
 
+  const calculateWinner = array => {
+    let lines = [[0, 1, 2], [0, 3, 6], [0, 4, 8], [1,4,7], [2, 4, 6], [2, 5, 8], [3,4,5], [6, 7 ,8]]
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i]
+      if (array[a] === 'X' && array[b] === 'X' && array[c] === 'X') {
+        return `${array[a]} Won!`
+      } else if (array[a] === 'O' && array[b] === 'O' && array[c] === 'O') {
+        return `${array[a]} Won!`
+      }
+  }
+  return 'Who will win?'
+  }
+
   return (
     <div className="App">
+      <span>{calculateWinner(gameSquares)}</span>
       <div className='container'>
         {gameSquares.map((square, index) => {
           return <Square 
